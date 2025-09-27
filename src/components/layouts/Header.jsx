e98bf6bde37366d2e7706481aeec6d1f4820ad6c
codayon <codayon@proton.me>
2025-09-27 16:52:04 +0600

    feat: add header for desktop

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,7 +1,70 @@
+import { NavLink } from "react-router";
+import Container from "../common/Container";
+import Flex from "../common/Flex";
+import {
+  BarsIcon,
+  CartIcon,
+  HeartIcon,
+  SearchIcon,
+  UomoLogo,
+  UserIcon,
+} from "../common/SvgAssets";
+
+const data = {
+  menu: [
+    { id: 1, to: "/", label: "HOME" },
+    { id: 2, to: "shop", label: "SHOP" },
+    { id: 3, to: "collection", label: "COLLECTION" },
+    { id: 4, to: "journal", label: "JOURNAL" },
+    { id: 5, to: "lookbook", label: "LOOKBOOK" },
+    { id: 5, to: "pages", label: "PAGES" },
+  ],
+};
+
 export default function Header() {
   return (
-    <>
-      <p>Header</p>
-    </>
+    <section className="py-8">
+      <Container>
+        <Flex className="justify-between">
+          <Flex className="gap-14">
+            <UomoLogo />
+            <ul className="flex items-center gap-10">
+              {data.menu.map((item) => (
+                <li key={item.id}>
+                  <NavLink
+                    to={item.to}
+                    className={({ isActive }) =>
+                      `relative group py-2 font-medium text-sm ${
+                        isActive ? "text-red-700" : "text-gray-500"
+                      }`
+                    }
+                  >
+                    {({ isActive }) => (
+                      <>
+                        {item.label}
+                        <span
+                          className={`absolute left-0 bottom-0 h-0.5 ${
+                            isActive
+                              ? "bg-red-700 w-full"
+                              : "bg-gray-500 w-0 group-hover:w-full transition-all ease-out duration-300"
+                          }`}
+                        ></span>
+                      </>
+                    )}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Flex className="gap-8">
+            <SearchIcon />
+            <UserIcon />
+            <HeartIcon />
+            <CartIcon />
+            <BarsIcon />
+          </Flex>
+        </Flex>
+      </Container>
+    </section>
   );
 }
