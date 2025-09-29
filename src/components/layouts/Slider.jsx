89e53ea622072e4cd7cbef470f18d47f21a1520b
codayon <codayon@proton.me>
2025-09-29 17:04:35 +0600

    chore: moving code for cleaner structure

---
+++ src/components/layouts/Slider.jsx
@@ -0,0 +1,63 @@
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaPinterest,
+  FaTwitter,
+} from "react-icons/fa";
+
+import Container from "../common/Container";
+import Button from "../common/Button";
+
+const data = {
+  social: [
+    { id: 1, icon: <FaFacebookF />, url: "https://www.facebook.com/" },
+    { id: 2, icon: <FaTwitter />, url: "https://www.twitter.com/" },
+    { id: 3, icon: <FaInstagram />, url: "https://www.instagram.com/" },
+    { id: 4, icon: <FaPinterest />, url: "https://www.pinterest.com/" },
+  ],
+};
+
+export default function Slider() {
+  return (
+    <section className="flex mb-20">
+      <div className="text-222222 flex flex-col items-center justify-center gap-y-14">
+        <div className="text-767676 flex flex-col gap-6">
+          {data.social.map((item) => (
+            <a
+              key={item.id}
+              href={item.url}
+              target="_blank"
+              className="hover:text-222222 cursor-pointer transition duration-300"
+            >
+              {item.icon}
+            </a>
+          ))}
+        </div>
+        <span className="text-767676 min-w-20 transform -rotate-90 text-sm font-medium">
+          FOLLOW US
+        </span>
+      </div>
+      <div className="border-f5e6e0 w-full p-2 border-2">
+        <div className="bg-f5e6e0 group">
+          <Container>
+            <div className="py-64">
+              <h4 className="text-c32929 before:bg-c32929 flex items-center before:block before:w-10 before:h-0.5 before:mr-2 text-sm font-medium">
+                NEW TREND
+              </h4>
+              <h1 className="max-w-3xl text-7xl leading-20 py-3">
+                SUMMER SALE STYLISH <span className="font-bold">WOMENS</span>
+              </h1>
+              <Button label="DISCOVER MORE" />
+            </div>
+          </Container>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-end">
+        <span className="before:bg-222222 flex items-center before:block before:w-8 before:h-0.5 before:mr-2 min-w-20 mb-10 transform -rotate-90 text-sm font-medium">
+          SCROLL
+        </span>
+      </div>
+    </section>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import Banner from "@/components/layouts/Banner";
 import Categories from "@/components/layouts/Categories";
 import DealOfTheWeek from "@/components/layouts/DealOfTheWeek";
+import Slider from "@/components/layouts/Slider";
 import TrendyProducts from "@/components/layouts/TrendyProducts";
 
 export default function Home() {
   return (
     <>
-      <Banner />
+      <Slider />
       <Categories />
       <TrendyProducts />
       <DealOfTheWeek />
