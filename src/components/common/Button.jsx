85042d4bfc7f7ee272cb0786bf58a6ba4039a273
codayon <codayon@proton.me>
2025-09-29 17:18:29 +0600

    feat: add span class name props

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,15 +1,24 @@
 import cn from "@/utils/cn";
 
-export default function Button({ className = "", label = "" }) {
+export default function Button({
+  btnClassName = "",
+  spanClassName = "",
+  label = "",
+}) {
   return (
     <button
       className={cn(
         "relative group py-2 cursor-pointer font-medium text-sm",
-        className
+        btnClassName
       )}
     >
       {label}
-      <span className="bg-black absolute left-0 bottom-0 w-3/5 h-0.5 group-hover:w-full transition-all ease-out duration-500"></span>
+      <span
+        className={cn(
+          "bg-black absolute left-0 bottom-0 w-3/5 h-0.5 group-hover:w-full transition-all ease-out duration-500",
+          spanClassName
+        )}
+      ></span>
     </button>
   );
 }
